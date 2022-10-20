d57488697ed49b5b826e4d4f7835544f6a06abc0
Fabio Barros <ph4byus@gmail.com>
2022-10-20 11:30:36 -0300

    resolvendo os BOs do gitaction e corrigindo branches

--- tests/test_load.py
+++ tests/test_load.py
@@ -4,7 +4,12 @@ from tests.constants import PEOPLE_FILE as PF
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load_positive_has_3_people():
     """Test load function"""
-    assert len(load(PF)) == 2
+    assert len(load(PF)) == 3
+
+@pytest.mark.unit
+@pytest.mark.medium
+def test_load_positive_first_name_starts_with_j():
+    """Test load function"""
     assert load(PF)[0][0] == 'J'
